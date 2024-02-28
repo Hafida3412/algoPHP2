1d0d870d59a328be535f55fd63b2859d1ac36028
Hafida 3412 <lounsy81@gmail.com>
2024-02-28 19:21:28 +0100

    exo3.php fini

--- exo3.php
+++ exo3.php
@@ -1,13 +1,14 @@
 <h1>Exercice 3</h1>
 
-<p>Afficher un lien hypertexte permettant d'accéder au site d'Elan Formation.
+<p>
+</p>Afficher un lien hypertexte permettant d'accéder au site d'Elan Formation.
     Le lien devra s'ouvrir dans un nouvel onglet(_blank).
-</p>
-
+<br>
 
 
 <a href = "https://elan-formation.fr/accueil" target="_blank">Elan</a>;
 
 <?php
 
-echo"<a href=$url>$site_title/a>";
+$url = "https://elan-formation.fr/accueil";
+   echo "<a href=$url'target='_blank">"Elan</a>";
