0c94316202f4654177bc8927c3d9b010a6031b5a
Hafida 3412 <lounsy81@gmail.com>
2024-02-29 16:23:03 +0100

    exo7.php fini

--- exo7.php
+++ exo7.php
@@ -7,12 +7,24 @@
     genererCheckbox($elements);
     //où $elements est un tableau associatif clé => valeur avec 3 entrées.
 
+    
 <?php
-$elements = array (
-    "$element 1"=> "coché",
-    "$element 2"=> "non coché",
-    "$element 3"=> ".");
 
+$elements = [
+    "Choix 1"=>"",
+    "Choix 2"=>"checked",
+    "Choix 3"=>""
+];
+
+function genererCheckbox($elements){
+    echo "<form>";
+    foreach ($elements as $choix=> $value){
+        echo  "<input type ='checkbox' name='".$choix."' value='".$choix."'  ". $value.">",
+        '<label for="'.$choix.'">'.$choix.'</label>';
+        
+        
+    }
+    echo "</form>";
+    
+}
 genererCheckbox($elements);
-function genererCheckbox($elements){}
-
