881d3f05a8bc469d311cd613b8efbc96c7781090
Hafida 3412 <lounsy81@gmail.com>
2024-02-29 16:56:07 +0100

    exo9.php

---
+++ exo9.php
@@ -0,0 +1,24 @@
+<h1>Exercice 9</h1>
+
+<p>Créer une fonction personnalisée permettant d'afficher des boutons radio avec
+    un tableau de valeurs en paramètre ("Monsieur", "Madame", "Mademoiselle").
+
+    Exemple: afficherRadio($nomsRadio);
+    
+
+
+<?php
+
+$nomsRadio = [
+    "Masculin" => "",
+    "Féminin" => "",
+    "Autre" => "checked"
+];
+Function afficherRadio($nomsRadio){
+    echo "<form>";
+    foreach($nomsRadio as $nom => $value){
+       echo "<input type='radio' name='".$nom."' value='$nom'"." $value"."/>";
+       '<label for="'.$nom.'">'.$nom.'</label>';
+    }
+    echo "</form>";
+}
