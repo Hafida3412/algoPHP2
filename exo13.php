91a59f23895ad16445f684b7401b5d94cb21b8f4
Hafida 3412 <lounsy81@gmail.com>
2024-03-04 14:36:59 +0100

    exo13.php fini

--- exo13.php
+++ exo13.php
@@ -26,7 +26,6 @@ class Voiture{
     private string $nbPortes;
     private int $vitesseActuelle;
     private bool $estDemarrer = false;
-    private bool $stopper = false;
 
 
 public function __construct (string $marque, string $modèle, string $nbPortes){
@@ -97,30 +96,22 @@ public function __construct (string $marque, string $modèle, string $nbPortes){
     }
 
     
-    public function getstopper()
-    {
-        return $this->stopper;
-    }
-    
-    public function setstopper($stopper)
-    {
-        $this->stopper = $stopper;
-        
-        return $this;
-    }
     public function demarrer(){
         $this->estDemarrer = true;
     return $this->marque. $this->modèle. " démarre.<br>";
     }
-
+    
     public function accelerer($vitesse){
          $this->vitesseActuelle += $vitesse;
-        //  $this->vitesseActuelle = $this->vitesseActuelle + $vitesse;
-    return $this->marque. $this->modèle. " accélère de:" .$this->vitesseActuelle. "km/h <br>";
+         //  $this->vitesseActuelle = $this->vitesseActuelle + $vitesse;
+         if($this->estDemarrer=false){
+            return "Le véhicule démarre";
+        }else{
+            return "Pour accélérer, il faut démarrer le véhicule:".$this->marque. $this->modèle. "<br>";
     }
-    
+}
     public function stopper(){
-        $this->stopper = true;
+        $this->estDemarrer = false;
         $this->vitesseActuelle = 0; 
         return $this->marque. $this->modèle. " est stoppé.<br>";
     }
@@ -133,14 +124,14 @@ public function __construct (string $marque, string $modèle, string $nbPortes){
     public function afficherInfos(){
     return "Nom et modèle du véhicule:" .$this->marque. $this->modèle.".<br> Nombre de portes:".$this->nbPortes.".<br> Le véhicule".$this->marque. "est".$this->estDemarrer.".<br> Sa vitesse actuelle est de:". $this->vitesseActuelle. "km/h<br>";    
 }  
+
     }
-
-
+    
 
 $V1 = new voiture("Peugeot", "408", "5");
 $V2 = new voiture("Citroen", "C4", "3");
 
-echo $V1->demarrer();
+// echo $V1->demarrer();
 echo "<br>";echo "<br>";
 //var_dump($V1->getEstDemarrer());
 echo $V1->accelerer(70);
@@ -149,6 +140,9 @@ echo "<br>";echo "<br>";
 echo $V1->afficherInfos();
 echo "<br>";echo "<br>";
 echo $V2->afficherInfos();
+echo "<br>"; echo "<br>";
+echo $V1->demarrer();
+echo "<br>"; echo "<br>";
 
 
 
