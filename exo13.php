71c5cc0493955db132e639a28651893d147387cb
Hafida 3412 <lounsy81@gmail.com>
2024-03-04 11:59:56 +0100

    exo13.php

--- exo13.php
+++ exo13.php
@@ -13,15 +13,21 @@
     réalisant des jeux de tests pour vérifier la cohérence de la classe Voiture.
     Vous devez afficher les tests et les éléments suivants:
 
-    Bonus: ajouter uen méthode ralentir(vitesse) dans la classe Voiture.
+    Bonus: ajouter une méthode ralentir(vitesse) dans la classe Voiture.
+    <br>
 
 <?php
+
+
 
 class Voiture{
     private string $marque;
     private string $modèle;
     private string $nbPortes;
-    private string $vitesseActuelle;
+    private int $vitesseActuelle;
+    private bool $estDemarrer = false;
+    private bool $stopper = false;
+
 
 public function __construct (string $marque, string $modèle, string $nbPortes){
     $this->marque = $marque;
@@ -30,7 +36,7 @@ public function __construct (string $marque, string $modèle, string $nbPortes){
     $this->vitesseActuelle = 0;
 }
 
-    public function getMarque()
+    public function getMarque(): string
     {
         return $this->marque;
     }
@@ -42,7 +48,7 @@ public function __construct (string $marque, string $modèle, string $nbPortes){
         return $this;
     }
 
-    public function getModèle()
+    public function getModèle(): string
     {
         return $this->modèle;
     }
@@ -54,7 +60,7 @@ public function __construct (string $marque, string $modèle, string $nbPortes){
         return $this;
     }
 
-    public function getNbPortes()
+    public function getNbPortes(): string
     {
         return $this->nbPortes;
     }
@@ -66,38 +72,84 @@ public function __construct (string $marque, string $modèle, string $nbPortes){
         return $this;
     }
 
-    public function getVitesseActuelle()
+    public function getVitesseActuelle(): int
     {
         return $this->vitesseActuelle;
     }
-
+    
     public function setVitesseActuelle($vitesseActuelle)
     {
         $this->vitesseActuelle = $vitesseActuelle;
-
+        
+        return $this;
+    }
+    
+    public function getEstDemarrer()
+    {
+        return $this->estDemarrer;
+    }
+    
+    public function setEstDemarrer($estDemarrer)
+    {
+        $this->estDemarrer = $estDemarrer;
+        
         return $this;
     }
 
+    
+    public function getstopper()
+    {
+        return $this->stopper;
+    }
+    
+    public function setstopper($stopper)
+    {
+        $this->stopper = $stopper;
+        
+        return $this;
+    }
     public function demarrer(){
-    return $this->marque. $this->modèle. $this->vitesseActuelle;
+        $this->estDemarrer = true;
+    return $this->marque. $this->modèle. " démarre.<br>";
     }
 
     public function accelerer($vitesse){
-    return $this->marque. $this->modèle. "accélère de ".($this->vitesseActuelle > 0). "km/h.";
+         $this->vitesseActuelle += $vitesse;
+        //  $this->vitesseActuelle = $this->vitesseActuelle + $vitesse;
+    return $this->marque. $this->modèle. " accélère de:" .$this->vitesseActuelle. "km/h <br>";
     }
     
     public function stopper(){
-    return $this->marque. $this->modèle. ($this->vitesseActuelle = 0). "est stoppé";
+        $this->stopper = true;
+        $this->vitesseActuelle = 0; 
+        return $this->marque. $this->modèle. " est stoppé.<br>";
     }
 
     public function ralentir($vitesse){
-    return $this->marque. $this->modèle."ralentit de ". ($this->vitesseActuelle < 0)."km/h";
+        $this->vitesseActuelle -= $vitesse;
+    return $this->marque. $this->modèle." ralentit.<br>";
     } 
-    public function afficherInfos() {
-        $result = "<h1>Infos véhicule v1</h1>";       
-}
-
-    public function __toString(){
-
+    
+    public function afficherInfos(){
+    return "Nom et modèle du véhicule:" .$this->marque. $this->modèle.".<br> Nombre de portes:".$this->nbPortes.".<br> Le véhicule".$this->marque. "est".$this->estDemarrer.".<br> Sa vitesse actuelle est de:". $this->vitesseActuelle. "km/h<br>";    
+}  
     }
-}
+
+
+
+$V1 = new voiture("Peugeot", "408", "5");
+$V2 = new voiture("Citroen", "C4", "3");
+
+echo $V1->demarrer();
+echo "<br>";echo "<br>";
+//var_dump($V1->getEstDemarrer());
+echo $V1->accelerer(70);
+echo $V1->ralentir(30);
+echo "<br>";echo "<br>";
+echo $V1->afficherInfos();
+echo "<br>";echo "<br>";
+echo $V2->afficherInfos();
+
+
+
+    
