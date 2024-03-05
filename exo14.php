3a224dcfa8f095c868a03da245fabf964480839f
Hafida 3412 <lounsy81@gmail.com>
2024-03-05 10:46:49 +0100

    exo14.php

--- exo14.php
+++ exo14.php
@@ -12,16 +12,16 @@
 
     Votre programme de test devra afficher les informations des 2 voitures de la façon suivante:
     
-    echo $v1->getInfos()."<br/>";
-    echo $ve1->getInfos()."<br/>";
+    echo $v1->getInfos()<br/>
+    echo $ve1->getInfos()<br/>
 
 
     <?php
-
+//CREATION DE LA CLASSE PARENT
 class Voiture{
     
-    private string $marque;
-    private string $modèle;
+    protected string $marque;//UTILISATION DE "PROTECTED" AU LIEU DE PRIVE POUR QUE CA S ETENDE 
+    protected string $modèle;
 
     public function __construct(string $marque, string $modèle){
         $this->marque = $marque;
@@ -52,12 +52,23 @@ class Voiture{
         return $this;
     }
 
-}
 
+    public function getInfos()
+    {
+        return $this->marque." ". $this->modèle;
+
+}
+}
+    //CREATION DE LA CLASSE ENFANT
 class VoitureElec extends Voiture{
+
     private string $autonomie;
 
-    public function __construct()
+    //FORMULE A UTILISER POUR FAIRE LE LIEN AVEC LA CLASSE PARENT
+    public function __construct(string $marque, string $modèle,int $autonomie){
+        parent::__construct($marque, $modèle);
+        $this->autonomie = $autonomie;
+    }
 
     public function getautonomie(): string
     {
@@ -70,17 +81,19 @@ class VoitureElec extends Voiture{
 
         return $this;
     }
-    }
 
-public function getInfos(){
-    return 
+    public function getInfos()
+    {
+        return parent::getInfos()." ". $this->autonomie;
+
 }
-    
-
-
+}
 
 $v1 = new Voiture("Peugeot", "408");
-$ve1 = new VoitureElec("BMW", "I3", "100");
+$ve1 = new VoitureElec("BMW", "I3","100");
+
+echo $v1->getInfos();
+echo $ve1->getInfos();
 
 
     
